7fe9f44d309bbe57d00076790193a25a15b035ff
David Champredon <davidchampredon@users.noreply.github.com>
2022-12-13 14:23:19 -0500

    basic repo structure

---
+++ R/foo.R
@@ -0,0 +1,18 @@
+
+#' @title Dummy test function
+#'
+#' @description Just a dummy test, this will be erased!
+#'
+#' @param x A numerical value
+#'
+#' @return a numerical value incremented by 1.
+#' @export
+#'
+#' @examples
+#' add_one(3.14)
+#'
+add_one <- function(x) {
+
+  return(x+1)
+
+}
