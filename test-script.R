eac078ee1c82ace330eb6e24e3be32ec8b89909d
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-01-19 13:22:37 -0500

    Create test-script.R
    Script for testing

---
+++ test-script.R
@@ -0,0 +1,33 @@
+siteid = 'TAB'
+date.start =  "2022-10-15"
+
+ww.conc = readRDS("../nml-azure-to-r/out/nml-database.RDS") %>%
+  clean_ww(gene_target = "covN2") %>%
+  filter(ww.loc == siteid, date >= date.start)
+
+# ---- Parameters ----
+
+
+prm.smooth = list(
+  window = 14,
+  align  = 'center',
+  method = 'loess',
+  span   = 0.20
+)
+
+dist.fec = ern::get_fecal_shedding('sarscov2')
+dist.gi  = ern::get_gi('sarscov2')
+
+# ---- Estimation ----
+
+r.estim = ern::estimate_R_ww(
+  ww.conc        = ww.conc,
+  dist.fec       = dist.fec,
+  dist.gi        = dist.gi,
+  scaling.factor = 2,
+  prm.smooth     = prm.smooth
+)
+
+
+g = ern::plot_diagnostic_ww(r.estim, caption = siteid)
+plot(g)
