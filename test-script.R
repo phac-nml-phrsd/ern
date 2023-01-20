efd5b302c38e2832718c3befab93fb8494ff9c40
David Champredon <davidchampredon@users.noreply.github.com>
2023-01-20 08:21:40 -0500

    temporary test scripts

--- test-script.R
+++ test-script.R
@@ -1,3 +1,6 @@
+library(ern)
+library(tidyverse)
+
 siteid = 'TAB'
 date.start =  "2022-10-15"
 
@@ -17,7 +20,8 @@ prm.smooth = list(
 
 dist.fec = ern::get_fecal_shedding('sarscov2')
 dist.gi  = ern::get_gi('sarscov2')
-
+dist.gi$sd <- 1
+dist.gi$sd_sd <- 0.1
 # ---- Estimation ----
 
 r.estim = ern::estimate_R_ww(
