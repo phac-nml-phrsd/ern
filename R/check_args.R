d365350c8388aa36c94f5153c14fd21b1edc163b
Irena Papst <papstirena@gmail.com>
2023-02-07 10:14:24 -0500

    switch to `method = non_parametric_si` in `EpiEstim::estimate_R()`
    fixes #69

--- R/check_args.R
+++ R/check_args.R
@@ -9,10 +9,10 @@ check_prm.R <- function(x){
   if(!is.null(x$config.EpiEstim)){
     message("-----
 You are passing your own config for EpiEstim::estimate_R().
-Please note that ern always uses method = 'si_from_sample',
+Please note that ern always uses method = 'non_parametric_si',
 and thus any method specified in your config will be ignored.
 Also, any config parameters that are specific to
-method = 'si_from_sample' (like n2) cannot be modified and
+method = 'non_parametric_si' (like si_distr) cannot be modified and
 will also be ignored.")
   }
 
