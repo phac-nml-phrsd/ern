82a1a64ee5912543ab4baae88a6a2b1849507b34
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-02-14 12:20:30 -0500

    Move fec shed dist testing to dist file

--- tests/testthat/test-get_fecal_shedding.R
+++
@@ -1,6 +0,0 @@
-test_that("get_fecal_shedding detects incorrect pathogen",
-          {
-            p = "sick"
-            expect_error(get_fecal_shedding(pathogen = p),
-                         "Pathogen not found. Aborting!")
-          })
