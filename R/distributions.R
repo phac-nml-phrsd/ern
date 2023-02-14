93f73f91a211427754c9ffcee1b90dcc0ad13d2d
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-02-14 11:31:57 -0500

    Rename func, move to dist file, add uncertainty vals
    Function renamed to maintain consistency with other dist functions. Inputted uncertainty values for mean, sd for sampling

--- R/distributions.R
+++ R/distributions.R
@@ -60,6 +60,69 @@ def_dist_reporting_fraction <- function(){
   # just a guess
 }
 
+#' @title Define fecal Shedding Distribution
+#'
+#' @param pathogen String. Name of the pathogen ('sarscov2', 'influenza', 'RSV')
+#' @param subtype String.
+#'
+#' @return A fecal shedding distribution stored as a list.
+#' @export
+#'
+#' @examples
+#' fec = get_fecal_shedding('sarscov2')
+#' print(fec)
+#'
+def_dist_fecal_shedding <- function(pathogen, subtype = '') {
+
+  # Check if correct pathogen specified
+  if(!isTRUE(pathogen %in% c('sarscov2', 'influenza', 'rsv'))){
+    stop("Pathogen not found. Aborting!")
+  }
+
+  fec = NULL
+
+  if(tolower(pathogen) == 'sarscov2'){
+    # Values obtained from Nourbakhsh et. al. (2021)
+    fec = list(
+      dist = "gamma",
+      mean = 14.04042,
+      mean_sd = 2,
+      # mean_sd & sd_sd are currently inputted values. TODO: Use MLE to get
+      # reliable estimates
+      sd = 10.07955,
+      sd_sd = 2,
+      max = 36
+    )
+
+    if(subtype == 'foo'){
+      # TO DO...
+    }
+  }
+
+
+  if(tolower(pathogen) == 'influenza'){
+    y = 1:15
+    z = y^2*exp(-y/2)
+    fec = z/sum(z)
+
+    if(subtype == 'foo'){
+      # TO DO...
+    }
+  }
+
+  if(tolower(pathogen) == 'rsv'){
+    y = 1:10
+    z = y^2*exp(-y/3)
+    fec = z/sum(z)
+
+    if(subtype == 'foo'){
+      # TO DO...
+    }
+  }
+
+  return(fec)
+}
+
 
 #' Draw from gamma based on parameter name in dist list
 #'
