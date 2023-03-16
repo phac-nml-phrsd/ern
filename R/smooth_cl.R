18f6f34c3e21f52595a6b553fbebc03784f64a52
Irena Papst <papstirena@gmail.com>
2023-03-16 09:50:56 -0400

    implementing centred rolling average for `smooth_cl()`

--- R/smooth_cl.R
+++ R/smooth_cl.R
@@ -9,25 +9,18 @@
 #' @export
 smooth_cl <- function(cl.daily, prm.smooth){
 
-  if(is.null(prm.smooth)) res = cl.daily
-
+  if(is.null(prm.smooth)) return(cl.daily)
+  # browser()
   if(!is.null(prm.smooth)){
-    res =   (cl.daily
-             %>% dplyr::group_by(.data$id)
-             %>% dplyr::mutate(
-               window = dplyr::case_when(
-                 t < prm.smooth$window ~ t,
-                 T ~ as.integer(prm.smooth$window)
-               )
-             )
-             %>% dplyr::mutate(
-               value = data.table::frollmean(
-                 .data$value, n = .data$window,
-                 align = "right", adaptive = TRUE)
-             )
-             %>% dplyr::select(-.data$window)
-             %>% dplyr::ungroup()
+    check_prm.smooth(prm.smooth)
+    df <- (cl.daily
+       %>% dplyr::group_by(.data$id)
+       %>% dplyr::mutate(
+         value = zoo::rollapply(
+           .data$value, width = prm.smooth$window,
+           FUN = mean, align = "center", partial = TRUE)
+       )
+       %>% dplyr::ungroup()
     )
   }
-  return(res)
 }
