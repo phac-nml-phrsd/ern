ea1e67ffc78efc700ee64b95708f67aa1cc54c59
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-03-29 13:37:28 -0400

    Switch to .onAttach, add runjags as suggest

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,13 @@
-.onLoad <- function(libname, pkgname){
-  j = runjags::testjags(silent = TRUE)
-  if(is.null(j)){
+.onAttach <- function(libname, pkgname){
+  suppressWarnings(j <- runjags::testjags(silent = TRUE))
+  if(isFALSE(j$JAGS.found)){
     stop("JAGS is not installed on this machine.
 Please install JAGS on https://sourceforge.net/projects/mcmc-jags/files/
 or request JAGS to be installed by your network administrator
 before loading package.")
   }
-  else{
-    message(
+  else if(isTRUE(j$JAGS.found)){
+    packageStartupMessage(
     paste("JAGS version", j$JAGS.version, "installed.\nrjags version", j$rjags.version, "installed."))
-
   }
 }
