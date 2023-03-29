2e2dc13c98e960eb85219702f43866dd62552dae
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-03-29 14:16:03 -0400

    Update zzz.R
    Stop users when using ern without attaching pkg to environment (i.e. ern::function())

--- R/zzz.R
+++ R/zzz.R
@@ -11,3 +11,13 @@ before loading package.")
     paste("JAGS version", j$JAGS.version, "installed.\nrjags version", j$rjags.version, "installed."))
   }
 }
+
+.onLoad <- function(libname, pkgname){
+  suppressWarnings(j <- runjags::testjags(silent = TRUE))
+  if(isFALSE(j$JAGS.found)){
+    stop("JAGS is not installed on this machine.
+Please install JAGS on https://sourceforge.net/projects/mcmc-jags/files/
+or request JAGS to be installed by your network administrator
+before loading package.")
+  }
+}
