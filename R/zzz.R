e7464903e76e4fb61283e387f5cf6708f21d0869
Warsame Yusuf <46528519+wyusuf068@users.noreply.github.com>
2023-04-13 10:19:45 -0400

    Update zzz.R
    Remove .onLoad to prevent duplicate messaging

--- R/zzz.R
+++ R/zzz.R
@@ -12,12 +12,3 @@ See README for more details.")
   }
 }
 
-.onLoad <- function(libname, pkgname){
-  suppressWarnings(j <- runjags::testjags(silent = TRUE))
-  if(isFALSE(j$JAGS.found)){
-    warning("JAGS is not installed on this machine but is required for Rt calculations on clinical testing data using ern::estimate_Rt_cl().
-To use this functionality, please install JAGS on https://sourceforge.net/projects/mcmc-jags/files/
-or request JAGS to be installed by your network administrator.
-See README for more details.")
-  }
-}
