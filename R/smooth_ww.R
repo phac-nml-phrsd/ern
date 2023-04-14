e64a9d72d14a20237b7a38d3c85256decb61dbf1
Irena Papst <papstirena@gmail.com>
2023-04-14 16:31:10 -0400

    try to write `smooth_with_x()` methods (BUGGED)

--- R/smooth_ww.R
+++ R/smooth_ww.R
@@ -7,7 +7,7 @@
 #'
 #' @export
 smooth_ww <- function(ww.conc, prm.smooth, silent = FALSE){
-
+  browser()
   # check args
   check_prm.smooth(prm.smooth)
 
@@ -16,37 +16,56 @@ smooth_ww <- function(ww.conc, prm.smooth, silent = FALSE){
     d = ww.conc %>%
       tidyr::complete(date = seq.Date(dplyr::first(date),
                                       dplyr::last(date), by = "day")) %>%
-      dplyr::mutate(value_smooth =
-                      zoo::rollmean(x = value,
-                                    k = prm.smooth$window,
-                                    align = prm.smooth$align,
-                                    fill  = NA,
-                                    na.rm = TRUE)) %>%
-      tidyr::drop_na(value_smooth) %>%
-      dplyr::mutate(t = as.numeric(date - dplyr::first(date)),
-                    obs = value_smooth)
+      smooth_with_rollmean(prm.smooth = prm.smooth) %>%
+      dplyr::mutate(
+        t = as.numeric(date - dplyr::first(date)),
+        obs = value
+        )
   }
 
   # loess
   if(prm.smooth$method == 'loess'){
-    t = ww.conc %>%
-      dplyr::mutate(x = as.numeric(date - min(date, na.rm = TRUE)+1))
-
-    z = stats::loess(formula = 'value ~ x', data = t, span = prm.smooth$span)
-    x = z$x[,1]
-    v = z$fitted
-
-    d = stats::approx(x=x, y = v, xout = 1:max(x)) %>%
-      as.data.frame() %>%
-      dplyr::rename(t = x, obs = y)
-
-    d[["date"]] = lubridate::ymd(min(t$date)) + d[["t"]]
+    d2 = smooth_with_loess(ww.conc, prm.smooth)
   }
 
   if(!silent){
     message(paste("Wastewater data smoothed using", prm.smooth$method,
                   "method"))
   }
+
+  return(d)
+}
+
+
+# helpers -----------------------------------------------------------------
+
+smooth_with_rollmean <- function(df, prm.smooth){
+  d <- (df %>%
+     dplyr::mutate(value_smooth =
+                     zoo::rollmean(x = value,
+                                   k = prm.smooth$window,
+                                   align = prm.smooth$align,
+                                   fill  = NA,
+                                   na.rm = TRUE)) %>%
+     tidyr::drop_na(value_smooth))
+
+  return(d)
+}
+
+smooth_with_loess <- function(df, prm.smooth) {
+  t = df %>%
+    dplyr::mutate(x = as.numeric(date - min(date, na.rm = TRUE)+1))
+
+  z = stats::loess(formula = 'value ~ x', data = t, span = prm.smooth$span)
+  x = z$x[,1]
+  v = z$fitted
+
+  d = stats::approx(x=x, y = v, xout = 1:max(x)) %>%
+    as.data.frame() %>%
+    ## TODO: EXTRACT THIS?
+    dplyr::rename(t = x, obs = y)
+
+  d[["date"]] = lubridate::ymd(min(t$date)) + d[["t"]]
 
   return(d)
 }
