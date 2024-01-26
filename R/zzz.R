ad3f32520f773137c8447e9e479658b3f1a9accc
David Champredon <david.champredon@gmail.com>
2024-01-26 13:38:46 -0500

    change messaging

--- R/zzz.R
+++ R/zzz.R
@@ -1,14 +1,25 @@
 .onAttach <- function(libname, pkgname){
+  
+  # `runjags`'s own check if JAGS is installed
   suppressWarnings(j <- runjags::testjags(silent = TRUE))
+  
+  
   if(isFALSE(j$JAGS.found)){
-    warning("JAGS is not installed on this machine but is required for Rt calculations on clinical testing data using ern::estimate_Rt_cl().
-To use this functionality, please install JAGS on https://sourceforge.net/projects/mcmc-jags/files/
-or request JAGS to be installed by your network administrator.
-See README for more details.")
+    msg = paste("\n --- WARNING ---\n",
+    "JAGS is not installed on this machine but is required\n",
+    "for Rt calculations on clinical testing data using \n", 
+    "ern::estimate_Rt_cl().\n",
+    "To use this functionality, please install JAGS from\n", 
+    "https://sourceforge.net/projects/mcmc-jags/files/ \n",
+    "or request JAGS to be installed by your network administrator.\n",
+    "See README for more details.")
   }
   else if(isTRUE(j$JAGS.found)){
-    packageStartupMessage(
-    paste("JAGS version", j$JAGS.version, "installed.\nrjags version", j$rjags.version, "installed."))
+    msg = paste(" -  JAGS version", j$JAGS.version, 
+                "installed.\n -  rjags version", j$rjags.version, 
+                "installed.")
   }
+  #packageStartupMessage(paste("Package `ern` version",packageVersion('ern')))
+  packageStartupMessage(msg)
 }
 
